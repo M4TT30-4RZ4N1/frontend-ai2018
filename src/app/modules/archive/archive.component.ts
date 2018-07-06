71ddca74e9a2fdd0d401565722e2c438534256df
Matteo Arzani <37015510+M4TT30-4RZ4N1@users.noreply.github.com>
2018-07-06 19:22:53 +0200

    Archive Design

--- src/app/modules/archive/archive.component.ts
+++ src/app/modules/archive/archive.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArchiveComponent implements OnInit {
 
+  items : Array<any> = new Array(4);
+
   constructor() { }
 
   ngOnInit() {
+    this.items.push('Antonio');
+    this.items.push('Matteo');
+    this.items.push('Raffaele');
+    this.items.push('Sabrina');
+  }
+
+  download(){
+    console.log('downloading');
+  }
+
+  remove(){
+    console.log('removing');
   }
 
 }
